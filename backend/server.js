d3c8085ada07ea0141f27c9b3fd79ecc3e27c19d
kshitij <21uec152@lnmiit.ac.in>
2025-03-17 23:14:19 +0530

    dev: products modal using mongoose: tested with postman

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './db.js';
-
+import { Product } from './products.modal.js';
 dotenv.config()
-const app = express();
+const app = express(); 
+
+//middleware - required for parsing req.body {line 18}
+app.use(express.json());
 
 app.listen(5000 , ()=>{
     connectDB()
     console.log('listening on port 5000');
 });
+
+
+//read through the below method, and try to self evaluate why did we fllow this order.
+app.post('/api/products', async (req, res) => {
+    const product = req.body;
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" })
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct,message: "Product Created" })
+    } catch (error) {
+        console.error("Error in Creating Product: ", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+
+})
+
 app.get('/', (req, res)=>{
     res.send("Welcome123")
 });
