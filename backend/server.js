2ea50b5d36f2a07c655adb0646249537b6e2162c
kshitij <21uec152@lnmiit.ac.in>
2025-03-18 20:15:48 +0530

    dev: delete product logic: tested with postman

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv'
 import { connectDB } from './db.js';
 import { Product } from './products.modal.js';
 dotenv.config()
-const app = express(); 
+const app = express();
 
 //middleware - required for parsing req.body {line 18}
 app.use(express.json());
 
-app.listen(5000 , ()=>{
+app.listen(5000, () => {
     connectDB()
     console.log('listening on port 5000');
 });
@@ -25,15 +25,25 @@ app.post('/api/products', async (req, res) => {
 
     try {
         await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct,message: "Product Created" })
+        res.status(201).json({ success: true, data: newProduct, message: "Product Created" })
     } catch (error) {
         console.error("Error in Creating Product: ", error.message)
         res.status(500).json({ success: false, message: "Server Error" })
     }
 
 })
-
-app.get('/', (req, res)=>{
+app.delete('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({ success: true, message: `Product with id: ${id} deleted successfully` })
+    } catch (error) {
+        console.log("Error while deleting: ", error.message)
+        res.status(404).json({ success: false, message: "Product not found" })
+    }
+})
+app.get('/', (req, res) => {
     res.send("Welcome123")
 });
 
