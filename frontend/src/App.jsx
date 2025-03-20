8a5d3e35b0306ace182d27c759ed0642a0c88f19
kshitij <21uec152@lnmiit.ac.in>
2025-03-21 02:27:20 +0530

    frontend setup: chakra ui version should be 2 for react 18 and above

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {Button} from '@chakra-ui/react'
+function App() {
+
+  return (
+    <>
+      
+      <h1>hi</h1>
+    
+  <Button color='blue'>hi this is a component of chakra ui</Button>
+    </>
+  )
+}
+
+export default App
