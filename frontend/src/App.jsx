954faa04afadc1dc3702b3794f0eb9f7b208ddfb
kshitij <21uec152@lnmiit.ac.in>
2025-05-04 22:33:06 +0530

    pushing code for reference

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import {Button} from '@chakra-ui/react'
-function App() {
+import { Button, Box } from '@chakra-ui/react'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import Navbar from './components/Navbar'
 
+//npm install react-router-dom
+import {Routes, Route } from "react-router-dom";
+
+function App() {
   return (
-    <>
-      
-      <h1>hi</h1>
     
-  <Button color='blue'>hi this is a component of chakra ui</Button>
-    </>
+      <Box minH={"100vh"}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/create' element={<CreatePage />} />
+        </Routes>
+      </Box>
   )
 }
 
-export default App
+export default App;
